a28c09fd91e94230d6e8b37aaa775aa1ad37e08c
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2023-12-12 14:05:35 -0300

    Now the first cycle only checkbox is hidden when using a subscription type coupon

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -5,8 +5,7 @@ jQuery( function ( $ ) {
 		renewals_field = $( '#wcs_number_payments' ),
 		allow_subscriptions_wrapper = $( '.subscription_coupon_allow_subscriptions_wrapper' ),
 		allow_subscriptions_field = $( '#subscription_coupon_allow_subscriptions' ),
-		first_cycle_only_wrapper = $( '.subscription_coupon_allow_subscriptions_first_cycle_only_wrapper' ),
-		first_cycle_only_field = $( '#subscription_coupon_first_cycle_only' );
+		first_cycle_only_wrapper = $( '.subscription_coupon_allow_subscriptions_first_cycle_only_wrapper' );
 
 
 	$( '#discount_type' ).on( 'change', function () {
@@ -17,11 +16,11 @@ jQuery( function ( $ ) {
 		update_status();
 	} );
 
-	function update_status () {
-		var selectedOption = $( '#discount_type' ).find(':selected' ).val();
+	function update_status() {
+		var selectedOption = $( '#discount_type' ).find( ':selected' ).val();
 		if ( [ 'fixed_cart', 'fixed_product', 'percent' ].indexOf( selectedOption ) >= 0 ) {
 			allow_subscriptions_wrapper.show();
-			if( allow_subscriptions_field.is( ':checked' ) ) {
+			if ( allow_subscriptions_field.is( ':checked' ) ) {
 				first_cycle_only_wrapper.show();
 			} else {
 				first_cycle_only_wrapper.hide();
@@ -29,6 +28,9 @@ jQuery( function ( $ ) {
 		} else {
 			allow_subscriptions_field.prop( 'checked', false );
 			allow_subscriptions_wrapper.hide();
+
+			first_cycle_only_wrapper.prop( 'checked', false );
+			first_cycle_only_wrapper.hide();
 		}
 	}
 
