e3983f2ca10da104629e01656506fe1efca0bc3d
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2023-12-19 14:19:21 -0300

    phpcs fixes

--- includes/class-mixed-coupons.php
+++ includes/class-mixed-coupons.php
@@ -2,10 +2,10 @@
 
 class Mixed_Coupons {
 	public static function init() {
-		add_filter( 'wcs_bypass_coupon_removal', [ __CLASS__, 'enable_subscription_coupon' ], 999, 5 );
-		add_action( 'woocommerce_coupon_options', [ __CLASS__, 'add_admin_coupon_fields' ], 10 );
-		add_action( 'woocommerce_coupon_options_save', [ __CLASS__, 'save_coupon_fields' ], 10 );
-		add_action( 'admin_enqueue_scripts', [ __CLASS__, 'enqueue_admin_scripts' ] );
+		add_filter( 'wcs_bypass_coupon_removal', array( __CLASS__, 'enable_subscription_coupon' ), 999, 5 );
+		add_action( 'woocommerce_coupon_options', array( __CLASS__, 'add_admin_coupon_fields' ), 10 );
+		add_action( 'woocommerce_coupon_options_save', array( __CLASS__, 'save_coupon_fields' ), 10 );
+		add_action( 'admin_enqueue_scripts', array( __CLASS__, 'enqueue_admin_scripts' ) );
 		remove_filter( 'woocommerce_coupon_is_valid_for_product', array( WC_Subscriptions_Coupon::class, 'validate_subscription_coupon_for_product' ), 10, 3 );
 		add_filter( 'woocommerce_coupon_is_valid_for_product', array( __CLASS__, 'validate_subscription_coupon_for_product' ), 10, 3 );
 	}
@@ -34,28 +34,28 @@ class Mixed_Coupons {
 
 		woocommerce_wp_checkbox(
 			array(
-				'id' => 'subscription_coupon_allow_subscriptions',
-				'label' => __('Allow Subscriptions', 'woocommerce-subscriptions'),
-				'desc_tip' => true,
-				'description' => __('When enabled will allow this coupon to be used in subscriptions.', 'woocommerce-subscriptions'),
+				'id'            => 'subscription_coupon_allow_subscriptions',
+				'label'         => __( 'Allow Subscriptions', 'woocommerce-subscriptions' ),
+				'desc_tip'      => true,
+				'description'   => __( 'When enabled will allow this coupon to be used in subscriptions.', 'woocommerce-subscriptions' ),
 				'wrapper_class' => 'subscription_coupon_allow_subscriptions_wrapper',
-				'value' => wc_bool_to_string( $coupon->get_meta( '_allow_subscriptions' ) ),
+				'value'         => wc_bool_to_string( $coupon->get_meta( '_allow_subscriptions' ) ),
 			)
 		);
 
 		woocommerce_wp_checkbox(
 			array(
-				'id' => 'subscription_coupon_first_cycle_only',
-				'label' => __('Limit discount to the first payment', 'woocommerce-subscriptions'),
-				'desc_tip' => true,
-				'description' => __('When enabled will not apply the coupon to renewals.', 'woocommerce-subscriptions'),
+				'id'            => 'subscription_coupon_first_cycle_only',
+				'label'         => __( 'Limit discount to the first payment', 'woocommerce-subscriptions' ),
+				'desc_tip'      => true,
+				'description'   => __( 'When enabled will not apply the coupon to renewals.', 'woocommerce-subscriptions' ),
 				'wrapper_class' => 'subscription_coupon_allow_subscriptions_first_cycle_only_wrapper',
-				'value' => wc_bool_to_string( $coupon->get_meta( '_apply_to_first_cycle_only' ) ),
+				'value'         => wc_bool_to_string( $coupon->get_meta( '_apply_to_first_cycle_only' ) ),
 			)
 		);
 	}
 
-	public static function save_coupon_fields ( $id ) {
+	public static function save_coupon_fields( $id ) {
 		// Check the nonce (again).
 		if ( empty( $_POST['woocommerce_meta_nonce'] ) || ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['woocommerce_meta_nonce'] ) ), 'woocommerce_save_data' ) ) {
 			return;
@@ -68,7 +68,7 @@ class Mixed_Coupons {
 	}
 
 	public static function enqueue_admin_scripts() {
-		wp_enqueue_script('subscription-coupon-admin', WOO_MIXED_COUPONS_URL . 'assets/js/admin.js', array('jquery'), '1.0', true);
+		wp_enqueue_script( 'subscription-coupon-admin', WOO_MIXED_COUPONS_URL . 'assets/js/admin.js', array( 'jquery' ), '1.0', true );
 	}
 
 	/**
